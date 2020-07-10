1f93e1068f664be6f44820171a60f08f6286d408
Justin Geeslin <justin@geesl.in>
2020-07-10 15:43:35 -0500

    Writing a CogTool XML file

--- src/main/java/balsamiq2cogtool/App.java
+++ src/main/java/balsamiq2cogtool/App.java
@@ -1,5 +1,6 @@
 package balsamiq2cogtool;
 
+import java.io.FileNotFoundException;
 import java.io.PrintStream;
 import java.sql.Connection;
 import java.sql.DriverManager;
@@ -9,10 +10,13 @@ import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.ArrayList;
 
-import org.sqlite.JDBC'
+import org.sqlite.JDBC;
 
-public class App 
+public class App
 {
+
+	private static Connection db;
+
     public static void main( String[] args )
     {
         System.out.println("Running..");
@@ -21,7 +25,7 @@ public class App
 					
 			/* Connect to sqlite database */
 			Class.forName("org.sqlite.JDBC");
-			Connection db = DriverManager.getConnection("jdbc:sqlite:TypeTesting.bmpr"); 
+			db = DriverManager.getConnection("jdbc:sqlite:TypeTesting.bmpr"); 
 			System.out.println("Opened a sql file..");
 					
 			db.close();
@@ -43,5 +47,35 @@ public class App
 			System.err.println(errMsg); 
 			main(null);
 		}
+
+		parseBalsamiqFile();
     }
+
+	private static void parseBalsamiqFile(){
+		PrintStream outputXML;
+		try {
+			outputXML = new PrintStream("cogtool.XML");
+		} catch (FileNotFoundException e) {
+			
+			String errMsg = e.getMessage();
+			System.err.println("File not found. " + errMsg); 
+			return;
+
+		} catch (Exception e) {
+			
+			String errMsg = e.getMessage();
+			System.err.println(errMsg); 
+			return;
+		}
+
+		System.out.println("About to write a CogTool XML...");
+		/*Create and open new movie.xml file*/
+		
+		outputXML.println("<?xml version=\"1.0\" encoding=\"UTF-8\"?>");
+		outputXML.println("<cogtoolimport version=\"1\">");
+
+		outputXML.println("</cogtoolimport>");
+		outputXML.close();
+
+	}
 }
